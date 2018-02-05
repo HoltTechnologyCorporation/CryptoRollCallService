d3026fb33f40ba1a8eebca4fdce9166d2dd5ac0c
shanganesh <shanganesh@ebay.com>
2018-02-04 22:12:41 -0800

    Added available coin exchanges

--- src/com/crc/models/CoinDetails.java
+++ src/com/crc/models/CoinDetails.java
@@ -16,73 +16,96 @@ public class CoinDetails {
 	private String facebookUrl;
 	private List<String> codeRepoLinks;
 	private String websiteUrl;
-	
+	private List<String> availableExchanges;
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getImageUrl() {
 		return imageUrl;
 	}
+
 	public void setImageUrl(String imageUrl) {
 		this.imageUrl = imageUrl;
 	}
+
 	public String getSymbol() {
 		return symbol;
 	}
+
 	public void setSymbol(String symbol) {
 		this.symbol = symbol;
 	}
+
 	public String getAlgorithm() {
 		return algorithm;
 	}
+
 	public void setAlgorithm(String algorithm) {
 		this.algorithm = algorithm;
 	}
+
 	public String getProofType() {
 		return proofType;
 	}
+
 	public void setProofType(String proofType) {
 		this.proofType = proofType;
 	}
+
 	public boolean isPreMined() {
 		return isPreMined;
 	}
+
 	public void setPreMined(boolean isPreMined) {
 		this.isPreMined = isPreMined;
 	}
+
 	public String getTwitterUrl() {
 		return twitterUrl;
 	}
+
 	public void setTwitterUrl(String twitterUrl) {
 		this.twitterUrl = twitterUrl;
 	}
+
 	public String getRedditUrl() {
 		return redditUrl;
 	}
+
 	public void setRedditUrl(String redditUrl) {
 		this.redditUrl = redditUrl;
 	}
+
 	public String getFacebookUrl() {
 		return facebookUrl;
 	}
+
 	public void setFacebookUrl(String facebookUrl) {
 		this.facebookUrl = facebookUrl;
 	}
+
 	public List<String> getCodeRepoLinks() {
 		return codeRepoLinks;
 	}
+
 	public void setCodeRepoLinks(List<String> codeRepoLinks) {
 		this.codeRepoLinks = codeRepoLinks;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	@Override
 	public String toString() {
 		return "CoinDetails [id=" + id + ", imageUrl=" + imageUrl + ", symbol=" + symbol + ", algorithm=" + algorithm
@@ -90,10 +113,20 @@ public class CoinDetails {
 				+ ", redditUrl=" + redditUrl + ", facebookUrl=" + facebookUrl + ", codeRepoLinks=" + codeRepoLinks
 				+ "]";
 	}
+
 	public String getWebsiteUrl() {
 		return websiteUrl;
 	}
+
 	public void setWebsiteUrl(String websiteUrl) {
 		this.websiteUrl = websiteUrl;
+	}
+
+	public List<String> getAvailableExchanges() {
+		return availableExchanges;
+	}
+
+	public void setAvailableExchanges(List<String> availableExchanges) {
+		this.availableExchanges = availableExchanges;
 	}
 }
